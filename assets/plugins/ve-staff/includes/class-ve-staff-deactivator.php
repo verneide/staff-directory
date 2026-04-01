825b88e159a1855187d3a71c26885038633c4c3a
verneide <39961267+verneide@users.noreply.github.com>
2026-04-01 12:11:27 -0500

    Fixed missing files

---
+++ assets/plugins/ve-staff/includes/class-ve-staff-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.sabers-design.com
+ * @since      1.0.0
+ *
+ * @package    Ve_Staff
+ * @subpackage Ve_Staff/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Ve_Staff
+ * @subpackage Ve_Staff/includes
+ * @author     Chase Sabers <csabers@verneide.com>
+ */
+class Ve_Staff_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
